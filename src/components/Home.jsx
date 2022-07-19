c921bb271c8e8d1447fc437ae7244c8d6f5c4bbe
manugarderes <69734460+manugarderes@users.noreply.github.com>
2022-07-19 14:01:38 +0200

    Infinite Slider

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import Lottie from "react-lottie";
 import * as animationData from "../loader.json";
 import { useNavigate } from "react-router-dom";
 import ProductItem from "./ProductItem";
-import CountUp from 'react-countup';
+import CountUp from "react-countup";
 
 function Home() {
   const navigate = useNavigate();
@@ -50,9 +50,129 @@ function Home() {
               src="https://www.retroka.com/wp-content/uploads/2021/10/rk_S21_web_HS_2_Oct21.gif"
             />
           </div>
+          <div className="infinite-body">
+            <div class="slider-infinite">
+              <div class="slide-track">
+                <div class="slide">
+                  <img
+                    src="https://www.gabatt.com/media/images/envio-1.png"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="https://www.nopcommerce.com/images/thumbs/0006813_mercadopago-checkout-latam-tecnofin.png"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="http://mconsulting.com.ec/wp-content/uploads/2019/01/Tarjetas-de-Cr%C3%A9dito.jpg"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="https://www.netflea.com/media/easycms/u/l/ulkkarikaupat_uusi_slider_1_.png"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="https://www.gabatt.com/media/images/envio-1.png"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="https://www.nopcommerce.com/images/thumbs/0006813_mercadopago-checkout-latam-tecnofin.png"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="http://mconsulting.com.ec/wp-content/uploads/2019/01/Tarjetas-de-Cr%C3%A9dito.jpg"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="https://www.netflea.com/media/easycms/u/l/ulkkarikaupat_uusi_slider_1_.png"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="https://www.gabatt.com/media/images/envio-1.png"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="https://www.nopcommerce.com/images/thumbs/0006813_mercadopago-checkout-latam-tecnofin.png"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="http://mconsulting.com.ec/wp-content/uploads/2019/01/Tarjetas-de-Cr%C3%A9dito.jpg"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="https://www.netflea.com/media/easycms/u/l/ulkkarikaupat_uusi_slider_1_.png"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="https://www.gabatt.com/media/images/envio-1.png"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+                <div class="slide">
+                  <img
+                    src="https://www.nopcommerce.com/images/thumbs/0006813_mercadopago-checkout-latam-tecnofin.png"
+                    height="100"
+                    width="250"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="counter">
             <div className="counterDiv">
-              <h1><CountUp end={10000} duration={10}/></h1>
+              <h1>
+                <CountUp end={10000} duration={10} />
+              </h1>
               <p>PRENDAS RECIRCULADAS</p>
             </div>
           </div>
