283dbb26bb9a4793a5218c155d9ca779bc4c3e22
manugarderes <69734460+manugarderes@users.noreply.github.com>
2022-08-03 13:03:33 -0300

    Contact Icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PhoneIcon from '@mui/icons-material/Phone';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Contact() {
   return (
     <div id="Contact">
       <div className="container">
         <div className="row">
-          <div className="col-md-6">
+          <div className="col-md-6 contactInfo">
             <h3>Encontranos en las redes sociales</h3>
             <hr />
-            <p>autentika@gmail.com</p>
-            <p>096 416 968</p>
-            <p>@autentika</p>
+            <p><LocationOnIcon/> Carlos Sáez 6418</p>
+            <p><PhoneIcon/> +598 97 748 613</p>
+            <p><InstagramIcon/> <a href="https://www.instagram.com/autentika.secondhand/">@autentika.secondhand</a></p>
           </div>
           <div className="col-md-6">
             <h3>Contactanos</h3>
